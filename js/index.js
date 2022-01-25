bb20802d6b22617ddab99aba51749d9d46654266
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-01-25 13:14:54 -0500

    working on spawning random cat images

--- js/index.js
+++ js/index.js
@@ -5,102 +5,105 @@ canvas.height = 600;
 canvas.width = 900;
 
 window.onload = () => {
-  document.getElementById("start-button").onclick = () => {
-    startGame();
-  };
+    document.getElementById("start-button").onclick = () => {
+        startGame();
+    };
 };
 
 const img = new Image();
 img.src = "../images/ufo.png";
 
 class Player {
-  constructor() {
-    this.x = canvas.width / 2 - 50;
-    this.y = canvas.height / 2 - 50;
-    this.driftX = 0;
-    this.driftY = 0;
-    this.w = 120;
-    this.h = 60;
-    this.angle = 0;
-    this.image = "../images/racecar.png";
-  }
-  //   newPos() {
-  //     this.x += this.speedX;
-  //     this.y += this.speedY;
-  //   }
+    constructor() {
+        this.x = canvas.width / 2 - 50;
+        this.y = canvas.height / 2 - 50;
+        this.driftX = 0;
+        this.driftY = 0;
+        this.w = 120;
+        this.h = 60;
+        this.angle = 0;
+        this.image = "../images/racecar.png";
+    }
 
-  move(direction) {
-    switch (direction) {
-      case "left":
-        if (this.x <= 10) {
-          this.x = 10;
-          this.driftX = 0;
-          this.driftY = 0;
-        } else {
-          this.x -= 15;
-          this.driftX = -0.6;
-        }
-        break;
-      case "right":
-        if (this.x + this.w >= canvas.width - 10) {
-          this.x = canvas.width - 10 - this.w;
-          this.driftX = 0;
-          this.driftY = 0;
-        } else {
-          this.x += 15;
-          this.driftX = 0.6;
-        }
-        break;
-      case "up":
-        if (this.y <= 10) {
-          this.y = 10;
-          this.driftX = 0;
-          this.driftY = 0;
-        } else {
-          this.y -= 15;
-          this.driftY = -0.6;
-        }
-        break;
-      case "down":
-        if (this.y + this.h >= canvas.height - 10) {
-          this.y = canvas.height - 10 - this.h;
-          this.driftX = 0;
-          this.driftY = 0;
-        } else {
-          this.y += 15;
-          this.driftY = 0.6;
+    move(direction) {
+        switch (direction) {
+            case "left":
+                if (this.x <= 10) {
+                    this.x = 10;
+                    this.driftX = 0;
+                    this.driftY = 0;
+                } else {
+                    this.x -= 15;
+                    this.driftX = -0.6;
+                }
+                break;
+            case "right":
+                if (this.x + this.w >= canvas.width - 10) {
+                    this.x = canvas.width - 10 - this.w;
+                    this.driftX = 0;
+                    this.driftY = 0;
+                } else {
+                    this.x += 15;
+                    this.driftX = 0.6;
+                }
+                break;
+            case "up":
+                if (this.y <= 10) {
+                    this.y = 10;
+                    this.driftX = 0;
+                    this.driftY = 0;
+                } else {
+                    this.y -= 15;
+                    this.driftY = -0.6;
+                }
+                break;
+            case "down":
+                if (this.y + this.h >= canvas.height - 10) {
+                    this.y = canvas.height - 10 - this.h;
+                    this.driftX = 0;
+                    this.driftY = 0;
+                } else {
+                    this.y += 15;
+                    this.driftY = 0.6;
+                }
         }
     }
-  }
 }
 
 class Item {
-  constructor(id) {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.w = 20;
-    this.h = 20;
-    this.points = this.generatePoints();
-    this.id = id;
-  }
-
-  generatePoints = () => {
-    return Math.floor(Math.random() * 5) + 1;
-  };
+    constructor(id) {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.w = 20;
+        this.h = 20;
+        this.points = 5;
+        this.id = id;
+        this.image = randomImage();
+        this.imageArr = [
+            "../images/cat1.png",
+            "../images/cat2.png",
+            "../images/cat3.png",
+            "../images/cat4.png",
+        ];
+    }
+    randomImage() {
+        this.imageArr[Math.floor(Math.random() * this.imageArr.length)];
+    }
 }
 
 let itemArr = [];
 
 function addItem() {
-  itemArr.push(new Item(1));
+    itemArr.push(new Item(1));
 }
 
 let score = 0;
 
 let time = 30;
+
 function timer() {
-  time -= 1;
-  console.log(time);
+    time -= 1;
+    console.log(time);
 }
 
 const driver = new Player();
@@ -108,97 +111,101 @@ const driver = new Player();
 // ENGINE
 //
 function startGame() {
-  setInterval(timer, 1000);
-  document.addEventListener("keydown", function (e) {
-    switch (e.code) {
-      case "ArrowLeft":
-        driver.move("left");
-        break;
-      case "ArrowRight":
-        driver.move("right");
-        break;
-      case "ArrowUp":
-        driver.move("up");
-        break;
-      case "ArrowDown":
-        driver.move("down");
-        break;
-    }
-  });
-  const itemInterval = setInterval(addItem, 1500);
+    setInterval(timer, 1000);
+    document.addEventListener("keydown", function(e) {
+        switch (e.code) {
+            case "ArrowLeft":
+                driver.move("left");
+                break;
+            case "ArrowRight":
+                driver.move("right");
+                break;
+            case "ArrowUp":
+                driver.move("up");
+                break;
+            case "ArrowDown":
+                driver.move("down");
+                break;
+        }
+    });
+    const itemInterval = setInterval(addItem, 1500);
 
-  animate();
+    animate();
 }
 
 const animate = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  driver.x += driver.driftX;
-  driver.y += driver.driftY;
-  ctx.drawImage(img, driver.x, driver.y, driver.w, driver.h);
-  ctx.fillStyle = "white";
-  ctx.font = "24px Arial";
-  ctx.fillText("Time:" + time, 15, 20);
-  ctx.fillText("Score:" + score, 15, 45);
-  detectWalls(driver);
+    driver.x += driver.driftX;
+    driver.y += driver.driftY;
+    ctx.drawImage(img, driver.x, driver.y, driver.w, driver.h);
+    ctx.fillStyle = "white";
+    ctx.font = "24px Arial";
+    ctx.fillText("Time:" + time, 15, 20);
+    ctx.fillText("Score:" + score, 15, 45);
+    detectWalls(driver);
 
-  for (let i = 0; i < itemArr.length; i++) {
-    const item = itemArr[i];
-    console;
-    ctx.fillStyle = "black";
-    ctx.fillRect(item.x, item.y, item.w, item.h);
-    detectCollision(driver, item);
-  }
-
-  function detectCollision(player, obj) {
-    if (
-      player.x < obj.x + obj.w &&
-      player.x + player.w > obj.x &&
-      player.y < obj.y + obj.h &&
-      player.y + player.h > obj.y
-    ) {
-      score += obj.points;
-      return true;
-    } else {
-      return false;
+    for (let i = 0; i < itemArr.length; i++) {
+        const item = itemArr[i];
+        console;
+        ctx.fillStyle = "black";
+        ctx.drawImage(item.image, item.x, item.y, item.w, item.h);
+        detectCollision(driver, item);
+        didCollide = detectCollision(driver, itemArr[i]);
+        if (didCollide) {
+            itemArr.splice(i, 1);
+        }
     }
-  }
-  if (time === 0) {
-    gameOver();
-  }
-  window.requestAnimationFrame(animate);
+
+    if (time === 0) {
+        gameOver();
+    }
+    window.requestAnimationFrame(animate);
 };
+
+function detectCollision(player, obj) {
+    if (
+        player.x < obj.x + obj.w &&
+        player.x + player.w > obj.x &&
+        player.y < obj.y + obj.h &&
+        player.y + player.h > obj.y
+    ) {
+        score += obj.points;
+        return true;
+    }
+}
+
 //
 // GAME OVER
 //
 function gameOver() {
-  obstArr = [];
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "red";
-  ctx.font = 50;
-  ctx.fillText("GAME OVER", canvas.width / 2 - 80, canvas.height / 2 - 30);
-  ctx.fillText("Score:" + score, canvas.width / 2 - 80, canvas.height / 2);
+    obstArr = [];
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "red";
+    ctx.font = 50;
+    ctx.fillText("GAME OVER", canvas.width / 2 - 80, canvas.height / 2 - 30);
+    ctx.fillText("Score:" + score, canvas.width / 2 - 80, canvas.height / 2);
 
-  window.cancelAnimationFrame(engine);
+    window.cancelAnimationFrame(engine);
 }
 
 const detectWalls = (player) => {
-  if (player.x < 10) {
-    player.x = 11;
-    player.driftX = 0;
-    player.driftY = 0;
-  }
-  if (player.x + player.w > canvas.width - 10) {
-    player.driftX = 0;
-    player.driftY = 0;
-  }
-  if (player.y < 10) {
-    player.driftX = 0;
-    player.driftY = 0;
-  }
-  if (player.y > canvas.height - player.h - 10) {
-    player.driftX = 0;
-    player.driftY = 0;
-  }
+    if (player.x < 10) {
+        player.x = 11;
+        player.driftX = 0;
+        player.driftY = 0;
+    }
+    if (player.x + player.w > canvas.width - 10) {
+        player.driftX = 0;
+        player.driftY = 0;
+    }
+    if (player.y < 10) {
+        player.driftX = 0;
+        player.driftY = 0;
+    }
+    if (player.y > canvas.height - player.h - 10) {
+        player.driftX = 0;
+        player.driftY = 0;
+    }
 };
